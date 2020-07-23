5d109816f407e9eefe71624503286113be29404b
Leo <leonardo161195@gmail.com>
2020-07-23 09:47:41 -0500

    Segundo commit

--- clima/clima.js
+++ clima/clima.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 
 const getClima = async(lat, lng) => {
 
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=40.750000&lon=-74.000000&appid=f538214095e778b9c3d883c8cbb6a075&units=metrics`)
-
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=f538214095e778b9c3d883c8cbb6a075&units=metric`)
+        // console.log(resp.data);
 
     return resp.data.main.temp;
 
--- lugar/lugar.js
+++ lugar/lugar.js
@@ -5,26 +5,38 @@ const getLugarLatLng = async(dir) => {
 
     const encodedUlr = encodeURI(dir);
 
-
     const instance = axios.create({
-        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodedUlr}`,
+        baseURL: `https://countries-cities.p.rapidapi.com/location/country/${encodedUlr}/geojson`,
         timeout: 1000,
         headers: { 'x-rapidapi-key': '2abb5978a4msha5a9c94526d8ef3p10db9ejsnf616da2e2126' }
     });
 
+    instance.get()
+        .then(resp => {
+            console.log(resp.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
     const resp = await instance.get();
 
-    if (resp.data.Results.length === 0) {
+    // if (resp.data.Results.length === 0) {
 
-        throw new Error(`No hay resultados para ${dir}`);
+    //     throw new Error(`No hay resultados para ${dir}`);
 
-    }
+    // }
 
 
-    const data = resp.data.Results[0];
-    const direccion = data.name;
-    const lat = data.lat;
-    const lng = data.lon;
+    const data = resp.data;
+    // console.log(data);
+    // console.log(JSON.stringify(data));
+    const direccion = data.code;
+    const lat = data.geo_json.coordinates[0][0][0][1];
+    const lng = data.geo_json.coordinates[0][0][0][1];
+    // const direccion = data.name;
+    // const lat = data.lat;
+    // const lng = data.lon;
 
     return {
         direccion,
